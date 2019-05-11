aa50fa6cb7198219d725cfcc4191f06dc2dd3af5
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-12 05:53:48 +0900

    Refactor test for elasticsearch

--- test/unit/test_elastichsearch.py
+++ test/unit/test_elastichsearch.py
@@ -8,25 +8,26 @@ def es():
     return es
 
 
-def test_zsh_history_index_exists(es):
-    index = es.indices.exists(index='zsh_history')
-    assert index is True
+class TestZshHistoryIndex(object):
+    def test_index_exists(self, es):
+        index = es.indices.exists(index='zsh_history')
+        assert index is True
 
+    def test_mapping_exists(self, es):
+        mapping = es.indices.get_mapping(index='zsh_history')
+        assert 'zsh_history' in mapping
 
-def test_zsh_history_mapping_exists(es):
-    mapping = es.indices.get_mapping(index='zsh_history')
-    assert 'zsh_history' in mapping
+    def test_mapping_has_timestamp_fields(self, es):
+        timestamp_mapping = es.indices.get_field_mapping(index='zsh_history',
+                                                         fields='@timestamp')
+        timestamp_type = (timestamp_mapping['zsh_history']['mappings']
+                          ['@timestamp']['mapping']['@timestamp']['type'])
+        assert timestamp_type == 'date'
 
-
-def test_zsh_history_mapping_has_correct_fields(es):
-    timestamp_mapping = es.indices.get_field_mapping(index='zsh_history',
-                                                     fields='@timestamp')
-    timestamp_type = (timestamp_mapping['zsh_history']['mappings']
-                      ['@timestamp']['mapping']['@timestamp']['type'])
-    timestamp_mapping = es.indices.get_field_mapping(index='zsh_history',
-                                                     fields='executed_command')
-    executed_command_type = (
-        timestamp_mapping['zsh_history']['mappings']['executed_command']
-        ['mapping']['executed_command']['type'])
-    assert timestamp_type == 'date'
-    assert executed_command_type == 'keyword'
+    def test_mapping_has_executed_command_fields(self, es):
+        executed_command_mapping = es.indices.get_field_mapping(
+            index='zsh_history', fields='executed_command')
+        executed_command_type = (
+            executed_command_mapping['zsh_history']['mappings']
+            ['executed_command']['mapping']['executed_command']['type'])
+        assert executed_command_type == 'keyword'
