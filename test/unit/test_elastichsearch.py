1293f7942fc6e221b8e605ec2aefde3b169f460d
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-15 08:40:24 +0900

    Add test for zsh-history index

--- test/unit/test_elastichsearch.py
+++ test/unit/test_elastichsearch.py
@@ -30,3 +30,48 @@ class TestZshHistoryFileIndex(object):
         field_type = (mapping['zsh_history']['mappings']['executed_command']
                       ['mapping']['executed_command']['type'])
         assert field_type == 'keyword'
+
+
+class TestZshHistoryIndex(object):
+    def test_index_exists(self, es):
+        index = es.indices.exists(index='zsh-history')
+        assert index is True
+
+    def test_mapping_exists(self, es):
+        mapping = es.indices.get_mapping(index='zsh-history')
+        assert 'zsh-history' in mapping
+
+    def test_mapping_has_timestamp_fields(self, es):
+        mapping = es.indices.get_field_mapping(index='zsh-history',
+                                               fields='@timestamp')
+        field_type = (mapping['zsh-history']['mappings']['@timestamp']
+                      ['mapping']['@timestamp']['type'])
+        assert field_type == 'date'
+
+    def test_mapping_has_executed_command_fields(self, es):
+        mapping = es.indices.get_field_mapping(index='zsh-history',
+                                               fields='executed_command')
+        field_type = (mapping['zsh-history']['mappings']['executed_command']
+                      ['mapping']['executed_command']['type'])
+        assert field_type == 'keyword'
+
+    def test_mapping_has_dir_fields(self, es):
+        mapping = es.indices.get_field_mapping(index='zsh-history',
+                                               fields='dir')
+        field_type = (mapping['zsh-history']['mappings']['dir']['mapping']
+                      ['dir']['type'])
+        assert field_type == 'keyword'
+
+    def test_mapping_has_host_fields(self, es):
+        mapping = es.indices.get_field_mapping(index='zsh-history',
+                                               fields='host')
+        field_type = (mapping['zsh-history']['mappings']['host']['mapping']
+                      ['host']['type'])
+        assert field_type == 'keyword'
+
+    def test_mapping_has_status_fields(self, es):
+        mapping = es.indices.get_field_mapping(index='zsh-history',
+                                               fields='status')
+        field_type = (mapping['zsh-history']['mappings']['status']['mapping']
+                      ['status']['type'])
+        assert field_type == 'short'
